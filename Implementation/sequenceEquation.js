4c354c070da725afc996169b88340295005cd6bb
Aditi Raj <aditir13@gmail.com>
2018-04-24 15:49:15 +0530

    Updated sequenceEquation.js

--- Implementation/sequenceEquation.js
+++ Implementation/sequenceEquation.js
@@ -1,12 +1,13 @@
 function permutationEquation(p) {
+    //Form an indexarray and find the index of x and then find the index of that value from the indexArray.
     var indexArray=[];
     var temp;
     var resArray=[];
     for(var i=0; i<p.length; i++){
         indexArray[p[i]]=i+1;
     }
-    for(var i=1; i<=p.length; i++){
-        temp= indexArray[i];
+    for(var x=1; x<=p.length; x++){
+        temp= indexArray[x];
          resArray.push(indexArray[temp]);     
     }
     return resArray;
