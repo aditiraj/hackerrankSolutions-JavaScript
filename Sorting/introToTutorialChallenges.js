d7928c23b34aef538f0feabce2beb8960cb936c0
Aditi Raj <aditir13@gmail.com>
2018-06-27 13:14:30 +0530

    Created introToTutorialChallenges.js

---
+++ Sorting/introToTutorialChallenges.js
@@ -0,0 +1,14 @@
+function processData(v,n,ar){
+// indexOf return the index of the element.
+    var index=ar.indexOf(v);
+    return index;
+}
+function main() {
+    var v = parseInt(readLine());
+    var n=  parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = processData(v,n,ar);
+    process.stdout.write("" + result + "\n");
+
+}
