94bf1b74cd2a2946aa9c49dc8661ce90da385730
Aditi Raj <aditir13@gmail.com>
2018-10-29 15:38:59 +0530

    Created helloWorld.js

---
+++ 10 Days of Javascript/helloWorld.js
@@ -0,0 +1,8 @@
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+    
+}
