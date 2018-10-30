e679016de5624ddab3fd1ae0cb7bddc7016d1b27
Aditi Raj <aditir13@gmail.com>
2018-10-30 11:31:19 +0530

    Created savethePrisoner.js

---
+++ Implementation/savethePrisoner.js
@@ -0,0 +1,26 @@
+function saveThePrisoner(n, m, s){
+    // Complete this function
+   var id;
+    id= s+(m-1);   
+    if(id>n){
+        var rem= id%n;
+        if(rem==0){
+            rem=n;
+        }
+       return rem;
+   }   
+    return id;   
+}
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var m = parseInt(n_temp[1]);
+        var s = parseInt(n_temp[2]);
+        var result = saveThePrisoner(n, m, s);
+        process.stdout.write(""+result+"\n");
+    }
+
+}
